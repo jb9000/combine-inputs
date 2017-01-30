14a018884e2b427d9cb15d22061b06893b867cfe
Jeff B <jeffbautista@users.noreply.github.com>
2017-01-30 14:07:30 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // Sample input: [[name, James Harden],[points, 27],[assists, 13],[rebounds, 10]]
-// Sample output: {{name: 'James Harden', points: 27, assists: 13, rebounds: 10}}
+// Sample output: {name: 'James Harden', points: 27, assists: 13, rebounds: 10}
 
 // This object runs a function when the HTML Convert button is clicked
 
@@ -7,12 +7,10 @@ var handlers = {
   convert: function() {
 
     var arrayInput = document.getElementById('arrayInput');
-    
     var newObject = {};
     
-    arrayInput.forEach(function(x) {
-      this.newObject[x[0]] = {}
-      this.newObject[x[0]] = x[1];
+    for (i=0; i<arrayInput.length; i++) {
+      newObject[x[0]] = x[1];
     });
     
     var outputDiv = document.getElementById('output');

36f951a229e8816c24c518151bb659934d3a8872
Jeff B <jeffbautista@users.noreply.github.com>
2017-01-30 20:33:19 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,27 +1,50 @@
-// Sample input: [[name, James Harden],[points, 27],[assists, 13],[rebounds, 10]]
-// Sample output: {name: 'James Harden', points: 27, assists: 13, rebounds: 10}
-
-// This object runs a function when the HTML Convert button is clicked
-
 var handlers = {
   convert: function() {
 
-    var nameInput = document.getElementById('nameInput').value;
-    var pointsInput = document.getElementById('pointsInput').value;
-    var assistsInput = document.getElementById('assistsInput').value;
-    var reboundsInput = document.getElementById('reboundsInput').value;
-    var teamArray = [];
-        
+    // Grab Input text
+    var nameInput = document.getElementById('nameInput');
+    var pointsInput = document.getElementById('pointsInput');
+    var assistsInput = document.getElementById('assistsInput');
+    var reboundsInput = document.getElementById('reboundsInput');
+    
+    // Build Object from Inputs
     var playerObject = {};
-    playerObject[name] = nameInput;
-    playerObject[points] = pointsInput;
-    playerObject[assists] = assistsInput;
-    playerObject[rebounds] = reboundsInput;
-    teamArray.push(playerObject);
+    playerObject['name'] = nameInput.value;
+    playerObject['points'] = pointsInput.valueAsNumber;
+    playerObject['assists'] = assistsInput.valueAsNumber;
+    playerObject['rebounds'] = reboundsInput.valueAsNumber;
+
+    // Build Array from Inputs
+    var playerArray = [];
+    playerArray.push(['Name', nameInput.value]);
+    playerArray.push(['Points', pointsInput.valueAsNumber]);
+    playerArray.push(['Assists', assistsInput.valueAsNumber]);
+    playerArray.push(['Rebounds', reboundsInput.valueAsNumber]);
+
+    // Build String from Object Properties
+    var outputString = '';
+    for (var prop in playerObject) {
+      outputString += prop + ": " + playerObject[prop] + ", ";
+    };
+    
+    // Build String from Array Elements
+    var outputString2 = '';
+    playerArray.forEach(function(x) {
+      outputString2 += x[0] + ': ' + x[1] + '<br>';
     });
     
-    console.log(teamArray);
-    var outputDiv = document.getElementById('output');
-    outputDiv.textContent = teamArray;
+    var outputDiv1 = document.getElementById('output1');
+    outputDiv1.innerHTML = '<h2>Object:</h2>' + JSON.stringify(playerObject);
+    
+    var outputDiv2 = document.getElementById('output2');
+    outputDiv2.innerHTML = '<h2>Array:</h2>' + JSON.stringify(playerArray);
+    
+    var outputDiv3 = document.getElementById('output3');
+    outputDiv3.innerHTML = '<h2>String:</h2>' + outputString2;
+    
+    nameInput.value = '';
+    pointsInput.value = '';
+    assistsInput.value = '';
+    reboundsInput.value = '';
   }
 };
